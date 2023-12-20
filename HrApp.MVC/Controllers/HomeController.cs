df30586c29ed503d4dfd2df472eca163f41e8ff8
Burak Ozkaya <ozkayaburak97@gmail.com>
2023-12-20 23:27:46 +0300

    api call changed

--- HrApp.MVC/Controllers/HomeController.cs
+++ HrApp.MVC/Controllers/HomeController.cs
@@ -9,42 +9,42 @@ using System.Text.Json;
 
 namespace HrApp.MVC.Controllers
 {
-    public class HomeController : Controller
-    {
-        public INotyfService _notifyService { get; }
+   public class HomeController : Controller
+   {
+      public INotyfService _notifyService { get; }
 
-        public HomeController(INotyfService notifyService)
-        {
-            _notifyService = notifyService;
-        }
+      public HomeController(INotyfService notifyService)
+      {
+         _notifyService = notifyService;
+      }
 
-        public async Task<IActionResult> Index()
-        {
-            using HttpClient client = new HttpClient();
+      public async Task<IActionResult> Index()
+      {
+         using HttpClient client = new HttpClient();
 
-            var response = await client.GetAsync("https://localhost:7213/api/AppUserHome/{Id}");
+         var response = await client.GetAsync("https://localhost:7213/api/User/AppUserHome/2e1b2611-f6cf-451d-9836-49f28b390f76");
 
-            if (response.StatusCode == System.Net.HttpStatusCode.OK)
-            {
-                var responseData = response.Content.ReadAsStringAsync().Result;
+         if (response.StatusCode == System.Net.HttpStatusCode.OK)
+         {
+            var responseData = response.Content.ReadAsStringAsync().Result;
 
-                var userHomeModel = JsonConvert.DeserializeObject<PersonelHomeViewModel>(responseData);
+            var userHomeModel = JsonConvert.DeserializeObject<PersonelHomeViewModel>(responseData);
 
-                _notifyService.Success("This is a success notification.");
+            _notifyService.Success("This is a success notification.");
 
-                return View(userHomeModel);
-            }
+            return View(userHomeModel);
+         }
 
-            _notifyService.Error("This is an error notification.");
+         _notifyService.Error("This is an error notification.");
 
-            return View();
-        }
+         return View();
+      }
 
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+      [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+      public IActionResult Error()
+      {
+         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+      }
+   }
 }
