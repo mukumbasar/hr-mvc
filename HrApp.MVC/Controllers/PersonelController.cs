ded8edb367c7f7348cbdf236ba8d33bfe814d3b2
yasaryildirim95 <yasaryildirim95@gmail.com>
2023-12-20 23:35:29 +0300

    Personel details action,view,viewmodel

--- HrApp.MVC/Controllers/PersonelController.cs
+++ HrApp.MVC/Controllers/PersonelController.cs
@@ -8,12 +8,35 @@ namespace HrApp.MVC.Controllers
    
     public class PersonelController : Controller
     {
-        
+
+        PersonelDetailViewModel user = new PersonelDetailViewModel() { 
+            Id = "1", 
+            Name = "Name1", 
+            Surname = "Surname1", 
+            Email = "Email1", 
+            Address = "Address1", 
+            BirthPlace = "BirthPlace1", 
+            BirthYear = DateTime.Now, 
+            Department = "Department1",
+            StartDate = DateTime.Now, 
+            TurkishIdentificationNumber = "12345678910",
+            IsActive = true,
+            CompanyName = "Koç",
+            Occupation = "Paspasçı",
+            MobileNumber = "053255323232"
+        };
+
 
         public IActionResult Index()
         {
             
             return View();
+        }
+
+
+        public IActionResult Details()
+        {
+            return View(user);
         }
 
     }
