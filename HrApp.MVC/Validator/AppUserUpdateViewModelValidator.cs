9afefbf3bb017144e862790cc2a7b7da7bb7f560
Burak Ozkaya <ozkayaburak97@gmail.com>
2023-12-21 15:48:40 +0300

    minor change

--- HrApp.MVC/Validator/AppUserUpdateViewModelValidator.cs
+++ HrApp.MVC/Validator/AppUserUpdateViewModelValidator.cs
@@ -10,7 +10,7 @@ namespace HrApp.MVC.Validator
         {
             RuleFor(y => y.Address).NotEmpty().WithMessage("Please enter your address.").MaximumLength(50).WithMessage("Address must be maximum of 50 characters.").MinimumLength(15).WithMessage("Address must be minimum of 15 characters.");
             RuleFor(y => y.MobileNumber).NotEmpty().WithMessage("Please enter your mobile phone number.").Length(11, 13).WithMessage("Please enter a valid phone number.");
-            RuleFor(y => y.UpdatedImage).NotEmpty().WithMessage("Please choose a photo.");
+            RuleFor(y => y.NewImage).NotEmpty().WithMessage("Please choose a photo.");
         }
     }
 }
