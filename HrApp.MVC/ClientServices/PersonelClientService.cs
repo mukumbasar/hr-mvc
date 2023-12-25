83bc691baf0414ed20682c23220085bbaa938a82
Burak Ozkaya <ozkayaburak97@gmail.com>
2023-12-25 10:53:17 +0300

    folder added

---
+++ HrApp.MVC/ClientServices/PersonelClientService.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http;
+
+namespace HrApp.MVC;
+
+public class PersonelClientService
+{
+    private readonly HttpClient _httpClient;
+    public PersonelClientService(IHttpClientFactory httpClientFactory)
+    {
+        _httpClient = httpClientFactory.CreateClient("api");
+    }
+
+}
