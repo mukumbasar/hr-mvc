de3e1a8fcfc34dcf2d53cf838333c4806c321b3e
Burak Ozkaya <ozkayaburak97@gmail.com>
2023-12-25 12:34:11 +0300

    Personnel client services are created and actions for personnel updated

--- HrApp.MVC/ClientServices/PersonelClientService.cs
+++ HrApp.MVC/ClientServices/PersonelClientService.cs
@@ -1,4 +1,8 @@
 ﻿using System.Net.Http;
+using System.Text;
+using HrApp.MVC.Helpers;
+using HrApp.MVC.Models;
+using Newtonsoft.Json;
 
 namespace HrApp.MVC;
 
@@ -9,5 +13,65 @@ public class PersonelClientService
     {
         _httpClient = httpClientFactory.CreateClient("api");
     }
+    public async Task<AppUserHomeViewModel> GetAppUserHomeViewModelAsync(string userId)
+    {
+        var response = await _httpClient.GetAsync($"User/{userId}");
 
+        if (response.StatusCode == System.Net.HttpStatusCode.OK)
+        {
+            var responseData = response.Content.ReadAsStringAsync().Result;
+
+            var userHomeModel = JsonConvert.DeserializeObject<AppUserHomeViewModel>(responseData);
+
+            return userHomeModel;
+        }
+
+        return null;
+    }
+    public async Task<AppUserDetailViewModel> GetAppUserDetailViewModelAsync(string userId)
+    {
+        var response = await _httpClient.GetAsync($"User/details/{userId}");
+
+        if (response.StatusCode == System.Net.HttpStatusCode.OK)
+        {
+            var responseData = response.Content.ReadAsStringAsync().Result;
+
+            var userDetailModel = JsonConvert.DeserializeObject<AppUserDetailViewModel>(responseData);
+
+            return userDetailModel;
+        }
+
+        return null;
+    }
+    public async Task<AppUserUpdateViewModel> GetAppUserUpdateAsync(string userId)
+    {
+        var response = await _httpClient.GetAsync($"User/details/{userId}");
+
+        if (response.StatusCode == System.Net.HttpStatusCode.OK)
+        {
+            var responseData = response.Content.ReadAsStringAsync().Result;
+
+            var userDetailModel = JsonConvert.DeserializeObject<AppUserUpdateViewModel>(responseData);
+
+            return userDetailModel;
+        }
+
+        return null;
+    }
+    public async Task<bool> UpdateAppUserUpdateViewModelAsync(AppUserUpdateViewModel appUserUpdateViewModel)
+    {
+        var bytes = await ImageConversions.ConvertToByteArrayAsync(appUserUpdateViewModel.NewImage);
+
+        appUserUpdateViewModel.UpdatedImage = bytes;
+
+        var response = await _httpClient.PutAsJsonAsync($"User", appUserUpdateViewModel);
+
+        if (response.IsSuccessStatusCode)
+        {
+
+            return true;
+        }
+
+        return false;
+    }
 }
