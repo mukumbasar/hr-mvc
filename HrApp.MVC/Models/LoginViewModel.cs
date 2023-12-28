88635c100044037ca6fde795cbe65e6b3a7d0085
Burak Ozkaya <ozkayaburak97@gmail.com>
2023-12-28 12:01:26 +0300

    new login page

--- HrApp.MVC/Models/LoginViewModel.cs
+++ HrApp.MVC/Models/LoginViewModel.cs
@@ -1,8 +1,13 @@
-﻿namespace HrApp.MVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HrApp.MVC.Models
 {
     public class LoginViewModel
     {
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(20, MinimumLength = 4, ErrorMessage = "Password must be between 4 and 20 characters")]
         public string Password { get; set; }
         public bool RememberMe { get; set; }
     }
