18aade9ef852cc44542eed16934620be56b91943
Burak Ozkaya <ozkayaburak97@gmail.com>
2023-12-29 17:48:13 +0300

    deneme

--- HrApp.MVC/Program.cs
+++ HrApp.MVC/Program.cs
@@ -28,7 +28,7 @@ var deneme = app.Environment.IsDevelopment();
 app.UseNotyf();
 
 // Configure the HTTP request pipeline.
-if (!isDevelopment)
+if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
