c5fa27bb5336c03ca280897d1dffb0e44e881402
Burak Ozkaya <ozkayaburak97@gmail.com>
2023-12-29 17:44:26 +0300

    minor changes

--- HrApp.MVC/Program.cs
+++ HrApp.MVC/Program.cs
@@ -9,18 +9,33 @@ builder.Services.AddMVCDependencies();
 builder.Services.AddControllersWithViews();
 GlobalOptions.Initialize(builder.Configuration);
 var app = builder.Build();
-
+app.Use(async (context, next) =>
+{
+    var env = app.Environment;
+    if (env.IsDevelopment())
+    {
+        // Eğer geliştirme ortamındaysa, özel bir mesaj gönder
+        await context.Response.WriteAsync("Bu uygulama Development ortamında çalışıyor.");
+    }
+    else
+    {
+        // Değilse, sonraki middleware'e devam et
+        await next();
+    }
+});
+var isDevelopment = app.Configuration["ASPNETCORE_URLS"]?.Contains("localhost") == true;
+var deneme = app.Environment.IsDevelopment();
 app.UseNotyf();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (!isDevelopment)
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+
 }
 
-// app.UseMiddleware<ExceptionHandlingMiddleware>();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
