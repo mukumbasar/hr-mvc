1272d8fe5c55b5503fddc2af198de4dcda9a5de2
Burak Ozkaya <ozkayaburak97@gmail.com>
2023-12-29 23:10:15 +0300

    new development environments added for project

--- HrApp.MVC/Program.cs
+++ HrApp.MVC/Program.cs
@@ -12,12 +12,11 @@ var app = builder.Build();
 app.UseNotyf();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (!app.Environment.IsDevelopment() && app.Environment.ApplicationName != "LocalTesting")
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-
 }
 
 app.UseHttpsRedirection();
