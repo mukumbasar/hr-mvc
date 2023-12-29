c4ba61c35b7ee03fade667f61b02222558febfbf
Burak Ozkaya <ozkayaburak97@gmail.com>
2023-12-29 22:56:33 +0300

    fluent validation logic changed

--- HrApp.MVC/Validator/CreateExpenseViewModelValidator.cs
+++ HrApp.MVC/Validator/CreateExpenseViewModelValidator.cs
@@ -7,10 +7,10 @@ namespace HrApp.MVC.Validator
     public class CreateExpenseViewModelValidator : AbstractValidator<CreateExpenseViewModel>
     {
 
-        public CreateExpenseViewModelValidator() 
+        public CreateExpenseViewModelValidator()
         {
-            
-            RuleFor(x => x.Amount).NotEmpty().WithMessage("Amount is required").LessThan(100000).WithMessage("Amount must be less then 100000.").GreaterThanOrEqualTo(1).WithMessage("Amount must be greater then 0.");
+
+            RuleFor(x => x.Amount).NotEmpty().GreaterThanOrEqualTo(1).WithMessage("Amount must be greater then 0.");
             RuleFor(x => x.CurrencyId).NotEmpty().WithMessage("Currency type is required");
             RuleFor(x => x.ExpenseTypeId).NotEmpty().WithMessage("Expense type is required");
             RuleFor(x => x.File).NotEmpty().WithMessage("File is required");
