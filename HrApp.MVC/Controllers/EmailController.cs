200952c7d4a6d70d35507ff2e48b038474de45cb
yasaryildirim95 <yasaryildirim95@gmail.com>
2024-01-02 19:49:36 +0300

    email controller

---
+++ HrApp.MVC/Controllers/EmailController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HrApp.MVC.Controllers
+{
+    public class EmailController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
