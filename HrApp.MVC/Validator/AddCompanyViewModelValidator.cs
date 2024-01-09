b6db7b8305cd97c614fa31a566eec1976ea0589e
yasaryildirim95 <yasaryildirim95@gmail.com>
2024-01-09 14:43:18 +0300

    mernisno validation

--- HrApp.MVC/Validator/AddCompanyViewModelValidator.cs
+++ HrApp.MVC/Validator/AddCompanyViewModelValidator.cs
@@ -8,7 +8,6 @@ namespace HrApp.MVC.Validator
         public AddCompanyViewModelValidator()
         {
             RuleFor(x => x.Name).NotEmpty().WithMessage("Company Name is required.");
-            RuleFor(x => x.MersisNo).NotEmpty().WithMessage("Mersis Number is required.");
             RuleFor(x => x.TaxNo).NotEmpty().WithMessage("Tax Number is required.");
             RuleFor(x => x.TaxOffice).NotEmpty().WithMessage("Tax Office is required.");
             RuleFor(x => x.File).NotEmpty().WithMessage("Please choose a file.");
@@ -27,6 +26,29 @@ namespace HrApp.MVC.Validator
                 .GreaterThan(x => x.ContractStartDate).WithMessage("Contract End Date must be greater than Contract Start Date.");
             RuleFor(x => x.IsActive).NotEmpty().WithMessage("Is Active is required.");
             RuleFor(x => x.CompanyTypeId).NotEmpty().WithMessage("Company Type is required.");
+            RuleFor(x => x.MersisNo).NotEmpty().WithMessage("Mersis Number is required.").Must((model, mersisNo) => IsAValidMersisNo(mersisNo, model.CompanyTypeId, model.TaxNo)).WithMessage("Invalid MERSİS Number.");
+        }
+
+        private bool IsAValidMersisNo(string mersisNo,int ctypeid,string taxno)
+        {
+            if (string.IsNullOrEmpty(mersisNo) || mersisNo.Length != 16)
+            {
+                return false;
+            }
+
+            if (ctypeid == 4 || ctypeid == 5 || ctypeid == 6) // şahıslar
+            {
+                return mersisNo.EndsWith("00015") || mersisNo.EndsWith("00019");
+            }
+            else if (ctypeid == 1 || ctypeid == 2) // anonim ve limited
+            {
+                return mersisNo.StartsWith($"0{taxno}") &&
+                       (mersisNo.EndsWith("00015") || mersisNo.EndsWith("00016") || mersisNo.EndsWith("00017"));
+            }
+            else
+            {
+                return true;
+            }
         }
     }
 }
