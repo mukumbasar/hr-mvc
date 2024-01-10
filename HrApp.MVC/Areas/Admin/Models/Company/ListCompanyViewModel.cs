29407c2656dcaa11a420d359570476f019bd10f4
yasaryildirim95 <yasaryildirim95@gmail.com>
2024-01-10 17:47:20 +0300

    m change

--- HrApp.MVC/Areas/Admin/Models/Company/ListCompanyViewModel.cs
+++ HrApp.MVC/Areas/Admin/Models/Company/ListCompanyViewModel.cs
@@ -6,7 +6,7 @@
         public string MersisNo { get; set; }
         public string TaxNo { get; set; }
         public string TaxOffice { get; set; }
-        public string ImageData { get; set; }
+        public byte[]? ImageData { get; set; }
         public string PhoneNumber { get; set; }
         public string Address { get; set; }
         public string EmailAddress { get; set; }
