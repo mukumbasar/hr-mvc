e8e2d50d039a71ddda2cd19f3bf2a11ad7d296f3
yasaryildirim95 <yasaryildirim95@gmail.com>
2024-01-11 21:57:16 +0300

    m change

--- HrApp.MVC/Areas/Admin/Models/Company/AddCompanyViewModel.cs
+++ HrApp.MVC/Areas/Admin/Models/Company/AddCompanyViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.Data;
+﻿using HrApp.MVC.CustomAttributes;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
 
 namespace HrApp.MVC.Areas.Admin.Models.Company
 {
@@ -6,11 +8,16 @@ namespace HrApp.MVC.Areas.Admin.Models.Company
     {
 
         public string Name { get; set; }
+        [MinLength(16), MaxLength(16)]
         public string MersisNo { get; set; }
+        [MaxLength(10)]
         public string TaxNo { get; set; }
         public string TaxOffice { get; set; }
+        [CheckExtension(new string[] { ".png", ".jpeg", ".jpg" })]
+        [CheckSize(1 * 1024 * 1024, ErrorMessage = "Maximum allowed file size is {0} bytes")]
         public IFormFile File { get; set; }
         public byte[]? ImageData { get; set; }
+        [MinLength(11)]
         public string PhoneNumber { get; set; }
         public string Address { get; set; }
         public string EmailAddress { get; set; }
--- HrApp.MVC/Areas/Admin/Models/Company/UpdateCompanyViewModel.cs
+++ HrApp.MVC/Areas/Admin/Models/Company/UpdateCompanyViewModel.cs
@@ -1,14 +1,22 @@
-﻿namespace HrApp.MVC.Areas.Admin.Models.Company
+﻿using HrApp.MVC.CustomAttributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace HrApp.MVC.Areas.Admin.Models.Company
 {
     public class UpdateCompanyViewModel
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        [MinLength(16), MaxLength(16)]
         public string MersisNo { get; set; }
+        [MaxLength(10)]
         public string TaxNo { get; set; }
         public string TaxOffice { get; set; }
+        [CheckExtension(new string[] { ".png", ".jpeg", ".jpg" })]
+        [CheckSize(1 * 1024 * 1024, ErrorMessage = "Maximum allowed file size is {0} bytes")]
         public IFormFile File { get; set; }
         public byte[]? ImageData { get; set; }
+        [MinLength(11)]
         public string PhoneNumber { get; set; }
         public string Address { get; set; }
         public string EmailAddress { get; set; }
