4e45fd1eeee74503f18d844852f40e6f96695790
burak özkaya <ozkayaburak97@gmail.com>
2024-01-11 22:37:11 +0300

    m change

--- HrApp.MVC/Models/Expense/ReadExpenseViewModel.cs
+++ HrApp.MVC/Models/Expense/ReadExpenseViewModel.cs
@@ -5,9 +5,13 @@ namespace HrApp.MVC.Models.Expense
     public class ReadExpenseViewModel
     {
         public int Id { get; set; }
+        public string Name { get; set; }
+        public string? SecondName { get; set; }
+        public string Surname { get; set; }
+        public string? SecondSurname { get; set; }
         public string ExpenseTypeName { get; set; }
         public decimal Amount { get; set; }
-        public string Currency {  get; set; }
+        public string Currency { get; set; }
         public string ApprovalStatus { get; set; }
         public DateTime ApprovalDate { get; set; }
         public string AppUserId { get; set; }
